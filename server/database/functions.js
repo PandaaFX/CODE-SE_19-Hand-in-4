e2111eff998b8479f9f28716389237d15031200a
PandaaFX <86691131+PandaaFX@users.noreply.github.com>
2026-04-08 01:10:29 +0200

    feat: add avatar update endpoint and db functions

--- server/database/functions.js
+++ server/database/functions.js
@@ -95,6 +95,17 @@ async function getUserData(sessionToken) {
   return rows[0];
 }
 
+async function getUserAvatar(sessionToken) {
+  const pool = getDBPool();
+
+  const [rows] = await pool.query(
+    "SELECT `avatar` FROM `users` WHERE session_token = ?",
+    [sessionToken],
+  );
+
+  return rows[0].avatar;
+}
+
 async function updateUserData(firstname, lastname, email, sessionToken) {
   const pool = getDBPool();
 
@@ -120,6 +131,17 @@ async function updateUserData(firstname, lastname, email, sessionToken) {
   const [rows] = await pool.query(
     `UPDATE \`users\` SET ${setClauses.join(", ")} WHERE \`session_token\` = ?;`,
     dynamicValues,
+  );
+
+  return rows.affectedRows > 0;
+}
+
+async function updateUserAvatar(avatarBlob, sessionToken) {
+  const pool = getDBPool();
+
+  const [rows] = await pool.query(
+    "UPDATE `users` SET `avatar` = ? WHERE `session_token` = ?;",
+    [avatarBlob, sessionToken],
   );
 
   return rows.affectedRows > 0;
@@ -156,7 +178,9 @@ module.exports = {
   addSessionTokenForUser,
   resetSessionTokenForUser,
   getUserData,
+  getUserAvatar,
   updateUserData,
+  updateUserAvatar,
   changeUserPassword,
   deleteUserAccount,
 };
