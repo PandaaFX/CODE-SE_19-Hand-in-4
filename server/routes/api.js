780291bde59e0ed411fe5fe9fd27d68502b516f1
PandaaFX <86691131+PandaaFX@users.noreply.github.com>
2026-04-08 01:11:03 +0200

    feat: add GET `api/avatar` endpoint

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const database = require("../database/functions");
 const router = express.Router();
+const path = require("path");
 
 router.get("/", function (_req, res) {
   res.status(200).send();
@@ -22,6 +23,27 @@ router.get("/getUserData", async function (req, res) {
     httpCode: 200,
     data: userData,
   });
+});
+
+const DEFAULT_AVATAR_PATH = path.resolve(
+  __dirname,
+  "../../public/assets/images/default-avatar.jpg",
+);
+router.get("/avatar", async function (req, res) {
+  const sessionToken = req.session.session_token;
+  if (!sessionToken) {
+    return res.status(401).send({
+      success: false,
+      httpCode: 401,
+      errorMessage: "Unauthorized",
+    });
+  }
+
+  const userAvatar = await database.getUserAvatar(sessionToken);
+  if (!userAvatar) return res.sendFile(DEFAULT_AVATAR_PATH);
+
+  res.contentType("image/webp");
+  return res.status(200).send(userAvatar);
 });
 
 router.get("/session", function (req, res) {
